dd08127c8aca9de38f5e534b8a4e3b762e1c65f9
holoed@users.noreply.github.com <edmondo@gmail.com>
2017-10-01 22:23:24 +0100

    Add clear

--- javascripts/CodeV4/Externals.js
+++ javascripts/CodeV4/Externals.js
@@ -331,6 +331,7 @@ var draw = function (size) {
 var clear = function (size) {
   var ctx = canvas.getContext('2d');
   context.clearRect(0, 0, size, size);
+  return "show:duration";
 }
 
 exports.clear = clear;
